7f4efff1b57fc7ce36c9bbf8e048c23f420926c1
Eli Pavlov <pavlov.eli@gmail.com>
2024-04-18 12:54:04 +0300

    version 11.15

--- app.py
+++ app.py
@@ -4,25 +4,25 @@ import random
 app = Flask(__name__)
 
 # List of compliments
-compliments = [
-    "You're amazing!",
-    "You're a superstar!",
-    "You're one in a million!",
-    "You're doing great!",
-    "You're fantastic!",
-    "You're a ray of sunshine!",
-    "You're simply the best!",
-]
-
 # compliments = [
-#     "Your automation skills are top-notch!",
-#     "You're a master of continuous integration and deployment!",
-#     "Your infrastructure as code is flawless!",
-#     "You're a wizard with container orchestration!",
-#     "Your monitoring and alerting setups are impressive!",
-#     "You make complex deployments look easy!",
-#     "Your DevOps practices are driving innovation and efficiency!",
+#     "You're amazing!",
+#     "You're a superstar!",
+#     "You're one in a million!",
+#     "You're doing great!",
+#     "You're fantastic!",
+#     "You're a ray of sunshine!",
+#     "You're simply the best!",
 # ]
+
+compliments = [
+    "Your automation skills are top-notch!",
+    "You're a master of continuous integration and deployment!",
+    "Your infrastructure as code is flawless!",
+    "You're a wizard with container orchestration!",
+    "Your monitoring and alerting setups are impressive!",
+    "You make complex deployments look easy!",
+    "Your DevOps practices are driving innovation and efficiency!",
+]
 
 @app.route('/')
 def index():
